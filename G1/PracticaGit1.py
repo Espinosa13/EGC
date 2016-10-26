484e7f4c84574a8481c524d64b03bcf871552e29
Antonio Espinosa Velasco <antonioespinosa13.ae@gmail.com>
2016-10-26 18:45:37 +0200

    Se ha modificado la funcion resta ademas de añadir nuevas pruebas

--- G1/PracticaGit1.py
+++ G1/PracticaGit1.py
@@ -1,8 +1,15 @@
 # -*- encoding: utf-8 -*-
 
 def resta(i, j):
-    return 2
+    return i-j
 
 if __name__=="__main__":
     if resta(8,6) == 2:
-        print("Correcto")
+        print("Correcto primera prueba")
+    else:
+        print("Incorrecto primera prueba")
+
+    if resta(7, 2) == 5:
+        print("Correcto segunda prueba")
+    else:
+        print("Incorrecto segunda prueba")
