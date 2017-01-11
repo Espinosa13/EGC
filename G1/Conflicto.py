4ed3b05ba3658e98acefaa0d6d21025f0301b6c7
Espinosa13 <antonioespinosa_13@hotmail.com>
2017-01-11 03:26:05 +0100

    Subida del archivo con el conflicto encontrado por git

---
+++ G1/Conflicto.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+import Math
+
+def resta(i, j):
+    return Math.resta(i,j)
+
+def suma(x, y):
+<<<<<<< HEAD
+    r = y-x
+    return r
+=======
+    result = x + y
+    return result
+>>>>>>> develop
+
+if __name__=="__main__":
+    if resta(8,6) == 2:
+        print("Correctamente ejecutara")
+        print("Correcto primera prueba satisfactoria")
+    else:
+        print("Incorrecto primera prueba")
+
+    if resta(7, 2) == 5:
+        print("Correcto segunda prueba")
+    else:
+        print("Incorrecto segunda prueba")
+
+    if resta(-3, 5) == -8:
+        print("Correcto tercera prueba")
+    else:
+        print("Incorrecto tercera prueba")
+
+    if suma(2, 2) == 4:
+        print("Correcto tercera prueba")
+    else:
+        print("Incorrecto tercera prueba")
