cc9d90ee65bc558602977037bdd5afdc50868e95
Espinosa13 <antonioespinosa_13@hotmail.com>
2017-01-11 02:47:40 +0100

    Borrada la función Resta

--- G1/PracticaGit1.py
+++ G1/PracticaGit1.py
@@ -1,27 +1,10 @@
 # -*- encoding: utf-8 -*-
 import Math
 
-def resta(i, j):
-    return Math.resta(i,j)
-
 def suma(x, y):
     return x+y
 
 if __name__=="__main__":
-    if resta(8,6) == 2:
-        print("Correcto primera prueba")
-    else:
-        print("Incorrecto primera prueba")
-
-    if resta(7, 2) == 5:
-        print("Correcto segunda prueba")
-    else:
-        print("Incorrecto segunda prueba")
-
-    if resta(-3, 4) == -8:
-        print("Correcto tercera prueba")
-    else:
-        print("Incorrecto tercera prueba")
 
     if suma(2, 2) == 4:
         print("Correcto tercera prueba")
